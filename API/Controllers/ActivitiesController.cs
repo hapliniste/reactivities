e851fe817fb7b7588ce5299a684769fd5bdfbe5d
Alexandre Jacquier <alexandre.jacquier@digitech-sa.ch>
2021-09-15 13:44:17 +0200

    React client and start of CQRS API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,32 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext context;
-
-        public ActivitiesController(DataContext context)
-        {
-            this.context = context;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await this.context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await this.context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{Id = id});
         }
     }
 }
---
+++ client-app/src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+      console.log(response);
+    })
+  }, [])
+
+
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+      <List>
+        {
+          activities.map((activity:any) => {
+            return(
+              <List.Item>
+                {activity.title}
+              </List.Item>
+            )
+          })
+        }
+      </List>
+    </div>
+  );
+}
+
+export default App;
